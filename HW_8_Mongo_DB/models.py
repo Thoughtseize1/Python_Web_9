718df0a0bb9a2503e68305e357ec3bde2c4d8875
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-05-04 23:07:00 +0200

    changed models for new req.

--- HW_8_Mongo_DB/models.py
+++ HW_8_Mongo_DB/models.py
@@ -5,11 +5,11 @@ from connect import connect
 class Authors(Document):
     fullname = StringField(required=True)
     born_date = StringField(max_length=50)
-    born_location = StringField(max_length=50)
+    born_location = StringField(max_length=150)
     description = StringField()
 
 
 class Quotes(Document):
-    tags = ListField(StringField(max_length=30))
-    quote = StringField(max_length=500, required=True)
+    tags = ListField(StringField(max_length=100))
+    quote = StringField(required=True)
     author = ReferenceField(Authors, required=True, reverse_delete_rule=CASCADE)
