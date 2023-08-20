aae9efacbd72efe34ed6593111b15d422a79ee04
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-08-20 18:23:34 +0200

    Update urls.py

--- HW_10_Django/hw_project/users/urls.py
+++ HW_10_Django/hw_project/users/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .forms import LoginForm
+from .views import RegisterView
 
 app_name = "users"
+
 urlpatterns = [
-    path("", views.main, name="root"),
+    path("signup/", RegisterView.as_view(), name="signup"),
+    path("signin/", LoginView.as_view(template_name='users/signin.html', authentication_form=LoginForm,
+                                      redirect_authenticated_user=True),
+         name="signin"),
+    path("logout/", LogoutView.as_view(template_name='users/logout.html'), name="logout")
 ]
