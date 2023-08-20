f3c1dd22e2daef04f344ddbaa624975e29a6aa5d
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-08-20 18:21:29 +0200

    Update extract.py

--- HW_10_Django/hw_project/useful_quotes/templatetags/extract.py
+++ HW_10_Django/hw_project/useful_quotes/templatetags/extract.py
@@ -6,18 +6,11 @@ from useful_quotes.utils import get_local_mongodb, print_msg
 register = template.Library()
 
 
-def get_author(id_):
-    db = get_local_mongodb()
-    author = db.authors.find_one({"_id": ObjectId(id_)})
-    return author["fullname"]
+@register.filter
+def get_author(author):
+    return author.fullname
 
 
-""" .для работы с SQL LITE """
-# def get_author(id_):
-#     try:
-#         author = Author.objects.get(id=id_)
-#         return author.fullname
-#     except Author.DoesNotExist:
-#         return None
-
-register.filter("author", get_author)
+@register.filter
+def extract_tags(quote):
+    return quote.tags.all()
